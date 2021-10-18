af33befcda1cfafe97994ad6bbb71fbae3a281a4
Adriano Pereira Junior <mrninhojr@gmail.com>
2021-10-17 21:10:48 -0300

    chore: Create base system

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios').default;
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/', async (req, res) => {
+  const { message } = req.body;
+  const { author } = req.headers;
+
+  if (!message) {
+    return res.status(400).json({ success: false, error: 'Message is required!' });
+  }
+
+  if (!author) {
+    return res.status(403).json({ success: false, error: 'Access denied!'})
+  }
+
+  try {
+    const response = await axios.post('http://localhost:3334/', {
+      user: author,
+    }); 
+
+    console.log(response.data);
+  } catch (err) {
+    console.error('Error while logging', err);
+  }
+
+  return res.json({ success: true, message });
+});
+
+app.listen(3333);
